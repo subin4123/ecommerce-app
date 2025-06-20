1a45206b161ddb0247fc35120331f144a577f26e
subin4123 <subindas4123@gmail.com>
2025-06-20 14:46:47 +0530

    updated

--- order/app.js
+++ order/app.js
@@ -3,7 +3,7 @@ const client = require('prom-client');
 const app = express();
 const port = process.env.PORT || 3000;
 client.collectDefaultMetrics();
-app.get('/', (req, res) => res.send('Hello from order service'));
+app.get('/', (req, res) => res.send('Hello from auth service'));
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
--- payment/app.js
+++ payment/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const client = require('prom-client');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Prometheus metrics
 client.collectDefaultMetrics();
+
 app.get('/', (req, res) => res.send('Hello from payment service'));
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
 });
+
 app.listen(port, () => console.log('Running on port', port));
+
